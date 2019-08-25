3dfd6103918b917ff96c9b8a0276d84d143d5a3e
Divya <enjamuridivya@gmail.com>
2019-08-25 12:50:51 -0700

    commandline arguments

---
+++ CommandLineArg/commandlinearg.js
@@ -0,0 +1,16 @@
+// var a = process.argv[2];
+// var b = process.argv[3];
+// if(a === b){
+// console.log(true);
+// }
+// else {
+//     console.log(false); 
+// }
+
+// if(parseFloat(a) && parseFloat (b) %7 === 0){
+// console.log("true");
+// }
+// (or)
+
+console.log((process.argv[2]) === (process.argv[3]));
+console.log((parseFloat(process.argv[2]) && parseFloat (process.argv[3]) % 7 === 0));
