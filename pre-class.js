66926efde0a7b6ed498019ce95908263c6a59431
Divya <enjamuridivya@gmail.com>
2019-08-25 12:59:48 -0700

    array of integers with duplicates

---
+++ pre-class.js
@@ -0,0 +1,54 @@
+// function removeDupes(arr){
+
+//     let finalArr = [];
+
+//     arr.forEach(function(content){
+
+//         if(finalArr.indexOf(content) === -1){
+
+//             finalArr.push(content);
+
+//         }
+
+//     });
+
+//     return finalArr;
+
+// }
+
+// ​
+
+// removeDupes([1,1,2,3,4,4,5]);
+var userArray = [1,2,2,4];
+
+var userArray2 = [4,5,4,4,7,5];
+
+var userArray3 = [1,2,3,5];
+
+​
+
+removeDups(userArray);
+
+removeDups(userArray2);
+
+removeDups(userArray3);
+
+​
+
+function removeDups(arr){
+
+    var tempArray = [];
+
+    for(var i=0; i < arr.length;i++){
+
+        if (!(tempArray.includes(arr[i]))){
+
+            tempArray.push(arr[i]);
+
+        }
+
+    }
+
+    console.log(tempArray);
+
+}
